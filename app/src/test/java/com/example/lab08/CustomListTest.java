509141866052400b3190f67e7cce75ef7be0a18c
ewanchukwilliam <ewanchukwilliam@gmail.com>
2026-03-06 11:11:52 -0700

    all the compilation stuff is tracked annoying

--- app/src/test/java/com/example/lab08/CustomListTest.java
+++ app/src/test/java/com/example/lab08/CustomListTest.java
@@ -4,4 +4,13 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 public class CustomListTest {
 
+    @Test
+    public void testHasCity() {
+        CustomList list = new CustomList();
+        City calgary = new City("Calgary", "AB");
+        list.addCity(calgary);
+        // hasCity() doesn't exist yet — this will fail (RED phase)
+        assertTrue(list.hasCity(calgary));
+    }
+
 }
